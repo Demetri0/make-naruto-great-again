90c5104e673d1f908ca95bbbdb37520e46f30f94
Demetri0 <d3i0@ya.ru>
2021-07-18 23:57:23 +0600

    Add video

--- app/src/lib/Video.svelte
+++ app/src/lib/Video.svelte
@@ -1,1 +1,28 @@
-<div>Video</div>
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    export let poster : string = '';
+    export let sources = [];
+    export let fragments = [];
+
+    let video;
+    onMount(() => {
+        console.log('video', video);
+    });
+</script>
+
+<div class="Video">
+    <video bind:this={video} poster={poster} controls>
+        {#each sources as source}
+            <source src={source.src} type={source.mimetype} label={`${source.quality}p`} />
+        {/each}
+    </video>
+</div>
+
+<style>
+    .Video {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
